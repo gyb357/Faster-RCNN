74b550869c65b275797a47aec65e3dd218876ca7
gyb357 <gyb357@naver.com>
2024-06-09 13:31:50 +0900

    no message

--- utils.py
+++ utils.py
@@ -1,6 +1,18 @@
-def operate(a: bool, b, c):
+from torch import Tensor
+import numpy as np
+import yaml
+
+
+def operate(a: bool, b, c) -> any:
     return b if a is True else c
 
-def operate_elif(a: bool, b, c: bool, d, e):
-    return b if a is True else operate(c, d, e)
+
+def tensor_to_numpy(tensor: Tensor) -> np.float32:
+    return tensor.detach().cpu().numpy().astype(np.float32)
+
+
+def load_config(path: str) -> dict:
+    with open(path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
